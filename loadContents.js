f6ea865c67c7cefbf6a3743373b417b52b352197
Adam Dobos <adam10603@gmail.com>
2017-02-06 11:58:39 +0100

    u work now ok

--- loadContents.js
+++ loadContents.js
@@ -38,7 +38,7 @@ function loadContents() {
             window.location = window.location + '#loaded';
             window.location.reload(true);
         }
-    }, 5000);
+    }, 500);
 }
 
 window.onload = loadContents;
